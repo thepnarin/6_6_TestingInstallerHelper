3e0bf6ee221fb837611bfde871029445ffdab554
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 23:44:01 +0700

    TestingInstallerHelper

---
+++ 6_6_TestingInstallerHelper/Mocking/InstallerHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_6_TestingInstallerHelper.Mocking
+{
+    public class InstallerHelper
+    {
+        private readonly IFileDownloader _fileDownloader;
+        private string _setupDestinationFile;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            _fileDownloader = fileDownloader;
+        }
+
+        public bool DownloadInstaller(string customerName, string installerName)
+        {
+            try
+            {
+                _fileDownloader.DownloadFile(
+                    string.Format("http://example.com/{0}/{1}",
+                        customerName,
+                        installerName),
+                    _setupDestinationFile);
+
+                return true;
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestNinja.UnitTests/InstallerHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using _6_6_TestingInstallerHelper.Mocking;
+using Moq;
+using NUnit.Framework;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd =>
+                fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadCompletes_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
